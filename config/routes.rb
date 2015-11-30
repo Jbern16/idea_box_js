22ddfd88ec0f67e845c4d6d4a6e4bbad5e0415e5
Regis <boudinot.regis@yahoo.com>
2015-11-30 14:27:53 -0700

    namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root 'ideas#index'
-  
+
+  namespace :api do
+    namespace :v1 do
+      resources :ideas, only: [:new, :destroy, :patch, :edit], defaults: { format: 'json' } do
+
+      end
+    end
+  end
 end
