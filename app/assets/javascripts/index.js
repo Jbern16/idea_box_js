3308bbdc9e137bf303a81938c8c119fd2e630cc2
selfup <boudinot.regis@yahoo.com>
2015-12-01 15:32:08 -0700

    delete implemented - wow

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,21 +1,22 @@
 'use strict'
 
 $(document).ready( () => {
-  $.getJSON('/api/v1/ideas')
-    .then(enumIdeas)
-  	.fail((data) => { alert('Something Went Wrong!') })
-    .always((data) => { console.log('Something Happened') })
+  loadIdeas()
+  deleteIdea()
 })
 
 var renderIdeas = (idea) => {
   $('#ideas').prepend(
-    "<h3 class='well'>"
-    + "Title: " + idea.title
-    + "</h3><p class='well'>"
-    + "Body: " + idea.body
-    + "</p>"
-    + "<button type='button' class='btn btn-danger'id='delete-idea'>Delete</button>"
-    + "<hr>"
+
+    `<div>`
+    + "<h3 class='well'>"
+    + `Title: ${idea.title}`
+    + `</h3><p class='well'>`
+    + `Body: ${idea.body}`
+    + `</p>`
+    + `<button type='button' class='btn btn-danger'id='delete-idea' data-id=${idea.id}>Delete</button>`
+    + `<hr>`
+    + `</div>`
   )
 }
 
