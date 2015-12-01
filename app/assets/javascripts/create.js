c69c7e99025423f5081e1db64f9f876e4a5a2052
selfup <boudinot.regis@yahoo.com>
2015-11-30 21:02:11 -0700

    able to create stuff

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -1,1 +1,37 @@
+var newIdea = () => {
+  $('#submit-idea').click( () => {
 
+    $.ajax({
+       method: "POST",
+       url: "api/v1/ideas",
+       data: {
+               title: ideaTitle(),
+               body: ideaBody()
+             },
+       success: () => {
+         loadIdeas()
+         clearForm()
+       },
+       error: () => {
+         alertError()
+       }
+    })
+  })
+}
+
+var ideaTitle = () => {
+  return $('#title-field').val()
+}
+
+var ideaBody = () => {
+  return $('#body-field').val()
+}
+
+var clearForm = () => {
+  $('#title-field').val("")
+  $('#body-field').val("")
+}
+
+var alertError = () => {
+  alert("negative ghost rider")
+}
--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 $(document).ready( () => {
+  newIdea()
   $.getJSON('/api/v1/ideas')
     .then(enumIdeas)
   	.fail((data) => { alert('Something Went Wrong!') })
