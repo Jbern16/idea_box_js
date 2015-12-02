eaa7d6309448b8b065e9f1c731932f49689384fe
selfup <boudinot.regis@yahoo.com>
2015-12-01 20:17:44 -0700

    can update - must refresh though

--- app/assets/javascripts/quality_update.js
+++ app/assets/javascripts/quality_update.js
@@ -3,14 +3,24 @@
 var updateQuality = () => {
   $('#ideas').delegate('#update-quality', 'click', (event) => {
 
-  let $idea = $(event.toElement)
+  let $idea           = $(event.toElement)
   let qualityUpdateId = $idea.attr('idea-id')
+  let qualityName     = $idea.attr('quality-value')
+  let qualityEnumNum  = 'Genius'
 
     $.ajax({
       type: 'PATCH',
       url: '/api/v1/ideas/' + qualityUpdateId,
+      data: {
+              // quality: 'Swill'
+              quality: thumbsUp(
+                qualityArray,
+                qualityName,
+                findQualityIndex(qualityArray, qualityName)
+              )
+            },
       success: () => {
-        loadIdeas()
+        fetchIdea(qualityUpdateId)
       },
       error: () => {
         loadIdeas()
@@ -22,10 +32,43 @@ var updateQuality = () => {
 
 var qualityArray = ['Swill', 'Plausible', 'Genius']
 
-// var
+var thumbsUp = (qualities, qualityName, functionPass) => {
+  let k = 0
+  let i = functionPass
 
-// var upgradeQuality = () => {
-//   if ()
+  if (qualityName === 'Genius') {
+    alert("Cannot upgrade further!")
+  } else {
+    let y = qualityArray[i += 1]
+    return y
+  }
+}
+
+
+var findQualityIndex = (array, findQual) => {
+  let j = 0
+    array.forEach( (quality, index) => {
+      if (findQual === quality) {
+        j = index
+      }
+  });
+  return j
+}
+
+// var thumbsDown = (qualities, qualityName) => {
+//   console.log(qualityName)
+//   let i = 0
+//   if (qualityName === 'Genius') {
+//     alert("Cannot upgrade further!")
+//   } else {
+//     qualityArray[i++]
+//   }
 // }
 
-// $idea.attr('quality-value')
+// var qualNum = (array, someFunc) => {
+//   array[someFunc]
+// }
+
+var fetchIdea = (updateId) => {
+  $.getJSON('/api/v1/ideas/' + updateId)
+}
--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::IdeasController < ApplicationController
   respond_to :json
 
+  def show
+    respond_with Idea.find(params[:id])
+  end
+
   def index
     respond_with Idea.all
   end
